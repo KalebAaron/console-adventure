f4ac6673ff4d08765f05a677af843c075a936449
Kaleb Hundersmarck <kalebaaron@live.com>
2017-06-13 17:32:18 -0600

    started models

--- models/Mansion.cs
+++ models/Mansion.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 namespace console_adventure
 {
     public class Mansion
     {
-      
+        public List<Room> Rooms = new List<Room>();
+
     }
+    public class Room
+{
+  public string description;
+  public List<Room> Exits = new List<Room>();
+}
 }
